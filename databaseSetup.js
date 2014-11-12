a76abf807a1adbbdd9012381261ff0249f0dbc18
Satoko <stogamori@gmail.com>
2014-11-12 11:25:36 -0800

    (init) add server files

---
+++ databaseSetup.js
@@ -0,0 +1,28 @@
+var mysql = require('mysql');
+// var fs = require('fs');
+
+var dbConnection;
+
+if (process.env.NODE_ENV === 'production') {
+  dbConnection = mysql.createConnection({
+    host: "====================.cloudapp.net",
+    user: "====================",
+    password: "====================",
+    database: "===================="
+  });
+} else {
+  dbConnection = mysql.createConnection({
+    user: "root",
+    password: "",
+    database: "==================="
+  });
+}
+
+dbConnection.connect();
+
+//exports.function....
+// exports.findUser = function(username, cb){
+//   dbConnection.query("SELECT userid as id from users where username = '" + username + "';", function(err, rows) {
+//     cb(err, rows);
+//   });
+// };
---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+var app = express();
+
+app.use(express.static(__dirname + '/app'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// connect to mysql database
+var connection = mysql.createConnection({
+	host: '',
+  user: '',
+  password:''
+}); 
+
+//user-login/signup routes
+app.post('/user/login', user.login);
+app.post('/user/signup', user.signup);
+
+//receivers route
+app.get('/receivers/getContacts', receivers.getContacts);
+app.post('/receivers/sendContent', receivers.sendContent);
+
+//pending route
+app.get('/pending/getPending', pending.getPending);
+app.post('/pending/sendVote', pending.sendVote);
+
+//results route
+app.get('/results/getResults', results.getResults);
+
+//server listen
+app.listen(process.env.PORT || '8080');
+
+module.exports = app;
