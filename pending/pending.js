97a48f8fe2a1f0c81c3bbbb82b2775aaf89b4bce
Satoko <stogamori@gmail.com>
2014-11-12 20:01:09 -0800

    (bug) change request and response to req and res

--- pending/pending.js
+++ pending/pending.js
@@ -1,23 +1,23 @@
 var dbConnection = require('../databaseSetup.js');
 
-module.exports.getPending = function(request, response){
-  var userId = request.body.userId;
+module.exports.getPending = function(req, res){
+  var userId = req.body.userId;
   //get contentId from receivers table with userId;
   //get contents data from contents table with contentId;
   //get pictureData from pictures table with pictureId;
   //get username from users table with userId;
-  getPendingConents(userId);
+  getPendingConents(userId, res);
 };
 
-module.exports.sendVote = function(request, response){
-	var userId = request.body.userId;
-	var contentId = request.body.contentId;
-	var vote = request.body.vote;
+module.exports.sendVote = function(req, res){
+	var userId = req.body.userId;
+	var contentId = req.body.contentId;
+	var vote = req.body.vote;
 
 	//look up the contents table with the contentId 
 	// if the vote is 1, store 1 in yes, if the vote is -1, store 1 in no
-	vote === 1 && saveYes(contentId);
-	vote === -1 && saveNo(contentId);
+	vote === 1 && saveYes(contentId, res);
+	vote === -1 && saveNo(contentId, res);
 
 	//delete connection in receivers table
   deleteFromReceivers(userId, contentId);
@@ -25,25 +25,25 @@ module.exports.sendVote = function(request, response){
 
 // ===================HELPER FUNCTIONS=======================
 
-var getPendingConents = function(userId){
+var getPendingConents = function(userId, res){
   dbConnection.query("SELECT p.pictureData, c.topic, u.username, c.contentId FROM receivers r JOIN contents c on r.contentID = c. contentID join pictures p on c.pictureID = p.pictureID join users u on c.userId = u.userId WHERE receiversId = '" + userId + "';", function(error, rows) {
     var data = { contents: [] };
     if(error){
-    	response.send(error);
+    	res.send(error);
     }else{
     	data.contents = rows;
-    	response.send(data);
+    	res.send(data);
     }
   });
 };
 
-var saveYes = function(contentId){
+var saveYes = function(contentId, res){
   dbConnection.query("UPDATE contents SET yes = yes+1 WHERE contentId = '" + contentId +"';", function(error) {
-    if(error){	response.send(error); }
+    if(error){	res.send(error); }
   });
 };
 
-var saveNo = function(contentId){
+var saveNo = function(contentId, res){
   dbConnection.query("UPDATE contents SET no = no+1 WHERE contentId = '" + contentId +"';", function(error) {
     if(error){	response.send(error); }
   });
